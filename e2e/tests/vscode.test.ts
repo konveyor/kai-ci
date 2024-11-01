016adddc2d4b4fe553bf9b2975b3617c513dd482
Shveta Sachdeva <sshveta@redhat.com>
2024-11-01 10:36:41 -0700

    Nightly CI for Vscode tests
    Signed-off-by: Shveta Sachdeva <sshveta@redhat.com>

--- e2e/tests/vscode.test.ts
+++ e2e/tests/vscode.test.ts
@@ -5,31 +5,27 @@ test.describe('VSCode Tests', () => {
   let vscodeApp: VSCode;
 
   test.beforeAll(async () => {
-    test.setTimeout(60000);
     const executablePath =
       process.env.VSCODE_EXECUTABLE_PATH || '/usr/share/code/code';
     vscodeApp = await VSCode.init(executablePath);
-    const window = vscodeApp.getWindow();
-    await window.screenshot({ path: 'vscode-initialized-screenshot.png' });
   });
 
   test('Should launch VSCode and check window title', async () => {
-    test.setTimeout(60000);
     const window = vscodeApp.getWindow();
     const title = await window.title();
-    await window.screenshot({ path: 'vscode-window-title-check.png' });
-    // expect(title).toContain('Visual Studio Code');
+    expect(title).toContain('Visual Studio Code');
+    await window.screenshot({ path: 'vscode-initialized-screenshot.png' });
   });
 
   test('Should open Extensions tab and verify installed extension', async () => {
     const window = vscodeApp.getWindow();
     const kaiTab = await window.getByRole('tab', { name: 'Konveyor' });
     await kaiTab.click();
-    // Assert if KAI explorer is opened.
     const title = window.getByRole('heading', {
       name: 'KAI',
       exact: true,
     });
     expect(title).toBeTruthy();
+    await window.screenshot({ path: 'kai-installed-screenshot.png' });
   });
 });
