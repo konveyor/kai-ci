aee0663da267c45722552a0b4991fccfbc35273e
Shveta Sachdeva <sshveta@redhat.com>
2024-11-11 15:40:48 -0800

    Method to clone coolstore repo and open vscode with it
    Signed-off-by: Shveta Sachdeva <sshveta@redhat.com>

--- e2e/utilities/utils.ts
+++ e2e/utilities/utils.ts
@@ -1,4 +1,11 @@
 import * as os from 'os';
+import * as fs from 'fs';
+import * as util from 'util';
+import { exec } from 'child_process';
+import * as path from 'path';
+
+const execPromise = util.promisify(exec);
+const repoDir = path.resolve('coolstore');
 
 // Function to get OS information
 export function getOSInfo(): string {
@@ -20,4 +27,16 @@ export function getKAIPluginName(): string {
   const vsixFileName =
     process.env.VSIX_FILE_NAME || 'konveyor-linux-0.0.1.vsix';
   return vsixFileName.replace(/(konveyor-)(\w+)(-.*)/, `$1${getOSInfo()}$3`);
+}
+
+export async function cleanupRepo() {
+  if (fs.existsSync(repoDir)) {
+    try {
+      await execPromise("rm -rf '${repoDir}'");
+    } catch (error) {
+      console.error('Error while cleaning up cloned repository:', error);
+    }
+  } else {
+    console.warn(`Directory ${repoDir} does not exist, skipping cleanup.`);
+  }
 }
