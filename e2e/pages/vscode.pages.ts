7ea579e1f3aa8ce872bac57e58e47c62a27f6365
midays <midays@redhat.com>
2025-01-30 11:58:24 +0200

    solution
    Signed-off-by: midays <midays@redhat.com>

--- e2e/pages/vscode.pages.ts
+++ e2e/pages/vscode.pages.ts
@@ -63,14 +63,9 @@ class VSCode {
       console.log('VSCode launched successfully, ensuring it stays open...');
       await new Promise(resolve => setTimeout(resolve, 5000));
 
-      console.log('vscode opened ...');
-
-      // console.log('Waiting for VSCode window to be ready...');
-      // await vscodeApp.waitForEvent('window', { timeout: 30000 });
       const window = await vscodeApp.firstWindow();
-      // await window.waitForLoadState('domcontentloaded');
-      // await window.waitForLoadState('load');
       return new VSCode(vscodeApp, window);
+
     } catch (error) {
       console.error('Error launching VSCode:', error);
       throw error;
