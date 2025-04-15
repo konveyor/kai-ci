6260f05c967412f589a678315ae4b95d2bdd3ee5
Alejandro Brugarolas <abrugaro@redhat.com>
2025-04-15 14:02:42 +0200

    upload report to aws s3 bucket
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

---
+++ kai-evaluator/utils/s3.ts
@@ -0,0 +1,31 @@
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
+
+export async function uploadObject(
+  file: string,
+  path: string,
+  contentType = 'application/json'
+) {
+  const client = new S3Client({ region: process.env.AWS_REGION });
+  return client.send(
+    new PutObjectCommand({
+      Key: path,
+      Body: file,
+      Bucket: process.env.S3_BUCKET_NAME,
+      ContentType: contentType,
+    })
+  );
+}
+
+export async function downloadObject(path: string) {
+  const client = new S3Client({ region: process.env.AWS_REGION });
+  return client.send(
+    new GetObjectCommand({
+      Key: path,
+      Bucket: process.env.S3_BUCKET_NAME,
+    })
+  );
+}
