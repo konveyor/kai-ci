1b4ad54a6279ee4b5c4a286d132bbc1b32330425
Alejandro Brugarolas <abrugaro@redhat.com>
2025-07-10 11:57:25 +0200

    aux
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- global.setup.ts
+++ global.setup.ts
@@ -2,9 +2,10 @@ import { VSCode } from './e2e/pages/vscode.pages';
 import { getOSInfo } from './e2e/utilities/utils';
 
 async function globalSetup() {
-  return;
+
   console.log('Running global setup...');
   const vscodeApp = await VSCode.init();
+  return;
   if (getOSInfo() === 'windows' && process.env.CI) {
     await vscodeApp.getWindow().waitForTimeout(30000);
   }
