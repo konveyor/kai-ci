5cf9215a8326479919331353a95379422ecc407c
Alejandro Brugarolas <abrugaro@redhat.com>
2025-07-11 16:08:29 +0200

    fix windows ci
    Signed-off-by: Alejandro Brugarolas <abrugaro@redhat.com>

--- global.setup.ts
+++ global.setup.ts
@@ -2,10 +2,11 @@ import { VSCode } from './e2e/pages/vscode.pages';
 import { getOSInfo } from './e2e/utilities/utils';
 
 async function globalSetup() {
+
   console.log('Running global setup...');
   const vscodeApp = await VSCode.init();
   if (getOSInfo() === 'windows' && process.env.CI) {
-    await vscodeApp.getWindow().waitForTimeout(30000);
+    await vscodeApp.getWindow().waitForTimeout(60000);
   }
 
   await vscodeApp.configureGenerativeAI();
